adbedf787a6df68d5f663188f135b6a72145020b
DonLyn <strangerathome@gmx.de>
2025-01-25 15:18:59 +0100

    removed print of entire text, added word and character report

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 def main():
+    file_contents = ""
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+    
+    #print(file_contents)
+    
+    length_in_words = len(file_contents.split())
+    #print(length_in_words)
+
+    character_count = {}
+
+    for character in file_contents:
+        if character.lower() in character_count:
+            character_count[character.lower()] += 1
+        else:
+            character_count.update({character.lower(): 1})
+
+    del character_count[" "]
+    del character_count["."]
+    del character_count["#"]
+    #del character_count["/n"]
+
+    print("- - - Begin report of books/frankenstein.txt - - -")
+    print(f"{length_in_words} words found in the document")
+    print()
+
+    for key  in character_count.keys():
+        print(f"The '{key}' character was found {character_count[key]} times")
+
+    print("- - - End report - - -")
+
+    
 
 main()
